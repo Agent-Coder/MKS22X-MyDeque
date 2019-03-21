2b5efedc718b66c8f5f809d5b5cb8605d3fcc4a7
Amanda Zheng <azheng00@stuy.edu>
2019-03-21 09:41:29 -0400

    did constructors for mydeque

---
+++ MyDeque.java
@@ -0,0 +1,25 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size, start, end;
+  @SuppressWarnings("unchecked")
+  public MyDeque(){
+    data = (E[])new Object[10];
+    start=0;
+    end=0;
+    size=0;
+  }
+  public MyDeque(int initialCapacity){
+    data = (E[])new Object[initialCapacity];
+    start=0;
+    end=0;
+    size=0;
+  }
+  public int size(){ }
+  public String toString(){ }
+  public void addFirst(E element){ }
+  public void addLast(E element){ }
+  public E removeFirst(E element){ }
+  public E removeLast(E element){ }
+  public E getFirst(E element){ }
+  public E getLast(E element){ }
+}
