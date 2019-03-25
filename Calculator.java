36ded7dcd6d5f76755415a52e5c7aa4ea58fb935
Amanda Zheng <azheng00@stuy.edu>
2019-03-25 10:57:03 -0400

    works for three simple test cases on website

--- Calculator.java
+++ Calculator.java
@@ -6,31 +6,39 @@ public class Calculator{
       String[] ary=s.split(" ",0);
       MyDeque<String> expression=new MyDeque<>(ary.length);
       for (int i=0;i<ary.length;i++) {
-        System.out.println(ary[i]);
         if(ary[i].equals("+")){
-          System.out.println(Integer.parseInt(expression.removeLast()));
-          expression.addLast(Integer.parseInt(expression.removeLast())+Integer.parseInt(expression.removeLast())+"");
+          double x=Double.parseDouble(expression.removeLast());
+          double y=Double.parseDouble(expression.removeLast());
+          expression.addLast(y+x+"");
         }
         else if(ary[i].equals("-")){
-          expression.addLast(Integer.parseInt(expression.removeLast())-Integer.parseInt(expression.removeLast())+"");
+          double x=Double.parseDouble(expression.removeLast());
+          double y=Double.parseDouble(expression.removeLast());
+          expression.addLast(y-x+"");
         }
         else if(ary[i].equals("*")){
-          expression.addLast(Integer.parseInt(expression.removeLast())*Integer.parseInt(expression.removeLast())+"");
+          double x=Double.parseDouble(expression.removeLast());
+          double y=Double.parseDouble(expression.removeLast());
+          expression.addLast(y*x+"");
         }
         else if(ary[i].equals("/")){
-          expression.addLast(Integer.parseInt(expression.removeLast())/Integer.parseInt(expression.removeLast())+"");
+          double x=Double.parseDouble(expression.removeLast());
+          double y=Double.parseDouble(expression.removeLast());
+          expression.addLast(y/x+"");
         }
         else if(ary[i].equals("%")){
-          expression.addLast(Integer.parseInt(expression.removeLast())%Integer.parseInt(expression.removeLast())+"");
+          double x=Double.parseDouble(expression.removeLast());
+          double y=Double.parseDouble(expression.removeLast());
+          expression.addLast(y%x+"");
         }
         else{
           expression.addLast(ary[i]);
         }
       }
-      return Integer.parseInt(expression.removeLast());
+      return Double.parseDouble(expression.removeLast());
     }
     public static void main(String[] args) {
-      System.out.println(eval("10 2.0 +"));
+      System.out.println(eval("10 2.0 -"));
       System.out.println(eval("11 3 - 4 + 2.5 *"));
       System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
     }
